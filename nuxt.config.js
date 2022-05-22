f90998835a60e72ee48ed84bccffa50873413ef1
robinlrx <robin@monkey-lab.io>
2022-05-22 18:21:15 +0200

    add target static to nuxt config for netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile:["three"],
-	target: 'static'
-  }
+    transpile:["three"]
+  },
+
+  target: 'static'
+  
 }
